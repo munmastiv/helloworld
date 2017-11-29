f28a239766c0e80a6ef64691d545fbd3d07dd678
munmastiv <tutatototatatam@gmail.com>
2017-11-28 19:21:23 -0800

    Add files via upload

---
+++ project001.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+#include<math.h>
+
+
+
+#defind n 40
+
+
+using namespace std;
+
+
+struct lenLop {
+	char buoiSang[5], buoiChieu[5];
+};
+
+struct dayinWeek {
+	lenLop thu2,thu3,thu4,thu5,thu6;
+};
+
+struct danhsachPhong {
+	char toaNha[10];
+	int soPhong;
+		
+};
